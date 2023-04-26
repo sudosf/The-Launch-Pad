1e4e6391e90850539e9ee61cda7d06fb0222647a
sudosf <sudosprogrammer@gmail.com>
2023-04-26 09:26:47 +0200

    added connection status check

--- scripts/Network.gd
+++ scripts/Network.gd
@@ -1,5 +1,20 @@
 extends Control
 
+"""
+	# global script for accessing variables from any scene and...
+	# connecting to API endpoints for DB access.
+"""
+
+var request_complete: bool = false
+var response # response data from API endpoints
+
+# questions from DB
+var multi_choice
+var multi_response
+var T_F # True/False
+var short_ans # Short answer
+var fill_blank # fill-in-the-blank
+
 var http_request : HTTPRequest = HTTPRequest.new()
 const SERVER_URL = "http://localhost:5000"
 
@@ -22,9 +37,10 @@ func _http_request_completed(_result, _response_code, _headers, _body):
 		printerr("Error w/ connection: " + String(_result))
 		return
 	
-	var response = parse_json(_body.get_string_from_utf8())
-	print(_response_code, "\n")
-	print(response)
+	response = parse_json(_body.get_string_from_utf8())
+	# print(_response_code)
+	# print(response, "\n")
+	request_complete = true
 
 """
 	# Available endpoints to use 
@@ -32,8 +48,16 @@ func _http_request_completed(_result, _response_code, _headers, _body):
 func getAll(): # get all questions data
 	var url_endpoint = SERVER_URL + "/questions"
 	send_request(url_endpoint)
-	
 
+func get_status():
+	var url_endpoint = SERVER_URL + "/"
+	send_request(url_endpoint) # request for DB status
+
+func print_status():
+	if (response['status'] == 1): 
+		print("DB Status: Connected!", "\nMessage: " + response['message'])
+	else: print("DB Status: Connection Failed.", "\nError: " + response['message'])
+	request_complete = false # reset request check
 
 
 
--- scripts/quiz.gd
+++ scripts/quiz.gd
@@ -3,5 +3,10 @@ extends Control
 onready var RestartButton = $VBoxContainer/next
 
 func _ready():
-	Network.getAll()
-	# print(Global.get_status())
+	Network.get_status()
+
+func _process(delta):
+	if Network.request_complete: # check if status reqeust is completed
+		Network.print_status()
+	else: return
+
