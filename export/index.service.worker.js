6806f83a7136a5ecc30c11a5ac135344c6307759
sudosf <sudosprogrammer@gmail.com>
2023-06-02 15:56:14 +0200

    export project

--- export/index.service.worker.js
+++ export/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1684930012|18110192";
+const CACHE_VERSION = "1685714153|3441087071";
 const CACHE_PREFIX = "The-Launch-Pad-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
